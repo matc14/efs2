5588d9da662ded79cafd5bd1e4572b8df17dd642
matc14 <100838929+matc14@users.noreply.github.com>
2024-03-17 01:17:41 +0100

    Add files via upload

---
+++ R/fs.relieff.R
@@ -0,0 +1,52 @@
+#' Build ReliefF Feature Selector
+#'
+#' @details
+#' Build ReliefF Feature Selector
+#'
+#' @param x input data where columns are variables and rows are observations (all numeric)
+#' @param y decision variable as a boolean vector of length equal to number of observations
+#' @param params method as accepted by \code{\link[stats]{p.adjust}} (\code{"BY"}
+#' is recommended for FDR, see Details)
+#' @return A \code{\link{data.frame}} with selected features and p.value
+#'
+#'
+#' @examples
+#' \dontrun{
+#'
+#' decisions <- data$class
+#' data$class <- NULL
+#'
+#' fs.mdfs.1D(data, decisions, params = list(feature.number = 100))
+#' }
+#'
+#'
+#' @export
+# fs.mdfs.1D <- function(x, y, params = list(adjust = 'holm', alpha = 0.05)){
+#   if (!is.data.frame(x)) data = as.data.frame(x)
+#   dim0 = 1
+#   div0 = 3
+#   adjust = params$adjust
+#   alpha = params$alpha 
+#   result = MDFS(data = x, decision = y, dimensions = dim0, divisions = div0, use.CUDA = FALSE,
+#                 p.adjust.method = adjust)
+#   var.names = names(x)
+#   index.imp = RelevantVariables(result$MDFS,
+#                                 level = alpha,
+#                                 p.adjust.method = adjust)
+#   var.imp.frame = data.frame(name = var.names,
+#                              Pvalue = result$p.value,
+#                              adjustPvalue = result$adjusted.p.value)[index.imp,]
+#   var.imp = var.imp.frame[order(var.imp.frame$adjustPvalue, decreasing = F),]
+#   return(var.imp)
+# }
+fs.relieff <- function(x, y, params = list(feature.number = 100)){
+  result <- fs.relief(x, y)
+  stats <- as.data.frame(result$stats)
+  var.names <- row.names(stats)
+  scores <- stats[,1]
+  var.imp <- as.data.frame(cbind(var.names, scores))
+  names(var.imp) <- c('name', 'score')
+  var.imp <- var.imp[order(var.imp$score, decreasing=T),][1:params$feature.number,]
+  return(var.imp)
+}
+
